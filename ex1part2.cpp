be4557ee4a807020aeab8a8b3a85f2f0b32c23a9
HeliJuS <103338394+HeliJuS@users.noreply.github.com>
2022-04-14 22:11:53 -0300

    Create ex1part2.cpp

---
+++ ex1part2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main() {
+	
+	char nome[150];
+	float salario, aumento;
+
+	printf("Informe seu nome: ");
+	gets_s(nome);
+
+	printf("Informe seu salario: ");
+	scanf_s("%f", &salario);
+
+	aumento = (1 * 2) * salario;
+
+	printf("O aumento do salario eh: %f", aumento);
+
+	system("PAUSE");
+	return(0);
+}
